bce48a3283bbfb8bd97a9f42219a0c15a0472ed4
Sungmin Cho <abelsteiger@gmail.com>
2019-10-03 15:15:12 -0700

    10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update10-03-19 / Class update

---
+++ others/anagram.py
@@ -0,0 +1,12 @@
+# Anagram exercise
+
+
+def is_anagram(A, B):
+    if len(A) == len(B):
+        for char in A:
+            if char not in B:
+                return False
+        return True
+
+
+print(is_anagram("listen", "silent"))
